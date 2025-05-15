4a4d022a9107d6e7b9f31aa6148464dc6523868a
Aymane <138299321+AymaneOnline@users.noreply.github.com>
2025-05-15 12:48:54 +0100

    message

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,14 +1,14 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'IT', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-    { id: 4, name: 'Charlie Brown', age: 32, department: 'Marketing', salary: 55000 },
-    { id: 5, name: 'Diana Prince', age: 29, department: 'IT', salary: 52000 },
-    { id: 6, name: 'Ethan Hunt', age: 40, department: 'Operations', salary: 70000 },
-    { id: 7, name: 'Felicity Smoak', age: 27, department: 'HR', salary: 48000 },
-    { id: 8, name: 'George Clooney', age: 45, department: 'Finance', salary: 80000 },
-    { id: 9, name: 'Hannah Montana', age: 22, department: 'Marketing', salary: 40000 },
-    { id: 10, name: 'Ian Malcolm', age: 50, department: 'Operations', salary: 90000 }
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'IT', salary: 45000, specialization: 'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
+    { id: 4, name: 'Charlie Brown', age: 32, department: 'Marketing', salary: 55000, specialization: 'Java' },
+    { id: 5, name: 'Diana Prince', age: 29, department: 'IT', salary: 52000, specialization: 'C#' },
+    { id: 6, name: 'Ethan Hunt', age: 40, department: 'Operations', salary: 70000, specialization: 'C++' },
+    { id: 7, name: 'Felicity Smoak', age: 27, department: 'HR', salary: 48000, specialization: 'Php' },
+    { id: 8, name: 'George Clooney', age: 45, department: 'Finance', salary: 80000, specialization: 'Dart' },
+    { id: 9, name: 'Hannah Montana', age: 22, department: 'Marketing', salary: 40000, specialization: 'PYthon' },
+    { id: 10, name: 'Ian Malcolm', age: 50, department: 'Operations', salary: 90000, specialization: 'Php' }
 ];
 
 // Function to display all employees
@@ -35,5 +35,15 @@ function findEmployeeById(employeeId) {
     }
     else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+    }
+}
+
+function findEmployeeBySpecialization(employeeSpecialization) {
+    const foundEmployee = employees.find(employee => employee.specialization === employeeSpecialization);
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} - ${foundEmployee.specialization}</p>`;
+    }
+    else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this Spacialization';
     }
 }
