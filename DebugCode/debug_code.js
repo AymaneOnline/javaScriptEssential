50f8f0beaf89f8f54d209e45bebb96aaa9f42ac1
Aymane <138299321+AymaneOnline@users.noreply.github.com>
2025-05-16 12:14:00 +0100

    message

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -5,7 +5,12 @@ function performOperation() {
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
     // Perform the operation
-        let result = multiply(num1, num2);
+        let multiplyResult = multiply(num1, num2);
+        let additionResult = add(num1, num2);
+        let substractionResult = substract(num1, num2);
+        let divisionResult = divide(num1, num2);
+
+        let result = [multiplyResult, additionResult, substractionResult, divisionResult];
 
         // Display the result
         displayResult(result);
@@ -22,9 +27,38 @@ function multiply(a, b) {
     return a * b;
 }
 
+function add(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+
+    // Multiply the numbers
+    return a + b;
+}
+
+function substract(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+
+    // Multiply the numbers
+    return a - b;
+}
+
+function divide(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+
+    // Multiply the numbers
+    return a / b;
+}
+
 function displayResult(result) {
     // Display the result in the paragraph element
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+    resultElement.innerHTML = `<ul>
+        <li>Multiplication: ${result[0]}</li>
+        <li>Addition: ${result[1]}</li>
+        <li>Substraction: ${result[2]}</li>
+        <li>Division: ${result[3]}</li>
+    </ul>`;
 }
         
